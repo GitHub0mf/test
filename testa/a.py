c950fe53922aebe5b3d850bca35df8bda03d5ab4
meifei.may000 <meifei.may000@bytedance.com>
2025-02-10 16:10:20 +0800

    a

--- testa/a.py
+++ testa/a.py
@@ -1,1 +1,6 @@
-adsdadsa
+def sort_output(arr):
+    # 对输入数组进行排序
+    sorted_arr = sorted(arr)
+    # 打印排序后的结果
+    for item in sorted_arr:
+        print(item)
---
+++ testa/aa.py
@@ -0,0 +1,8 @@
+# 定义排序函数
+def sort_array(arr):
+    return sorted(arr)
+
+# 示例数组
+numbers = [5, 2, 8, 1, 9]
+sorted_numbers = sort_array(numbers)
+print('排序后的数组:', sorted_numbers)
